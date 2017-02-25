4ce5b6bcac6602deaf29d9990f035639d929837b
Olivier Marino <olivier.marino.om@gmail.com>
2017-02-25 23:08:55 +0100

    Ajout du framework W

---
+++ app/Views/default/home.php
@@ -0,0 +1,6 @@
+<?php $this->layout('layout', ['title' => 'Accueil']) ?>
+
+<?php $this->start('main_content') ?>
+	<h2>Vod in.</h2>
+	<p>Vous avez atteint la page d'accueil. Bravo.</p>
+<?php $this->stop('main_content') ?>
