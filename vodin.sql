476659357a60a53be53e467ce7caa11535e5f8ca
isabelle <cuda.isabelle@orange.fr>
2017-02-27 15:17:39 +0100

    ajout export BDD

---
+++ vodin.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 24 Février 2017 à 16:27
+-- Version du serveur :  10.1.19-MariaDB
+-- Version de PHP :  5.5.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `vodin`
+--
+CREATE DATABASE IF NOT EXISTS `vodin` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `vodin`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `platforms`
+--
+
+DROP TABLE IF EXISTS `platforms`;
+CREATE TABLE `platforms` (
+  `idPlatform` int(11) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `logo` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `styles`
+--
+
+DROP TABLE IF EXISTS `styles`;
+CREATE TABLE `styles` (
+  `idStyle` int(11) NOT NULL,
+  `fileName` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `idUser` int(11) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `pseudo` varchar(100) NOT NULL,
+  `uuid` varchar(250) NOT NULL,
+  `lastConnexion` datetime NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `pwdToken` varchar(100) NOT NULL,
+  `settings` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_platforms`
+--
+
+DROP TABLE IF EXISTS `users_platforms`;
+CREATE TABLE `users_platforms` (
+  `idUser` int(11) NOT NULL,
+  `idPlatform` int(11) NOT NULL,
+  `apiKey` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `platforms`
+--
+ALTER TABLE `platforms`
+  ADD PRIMARY KEY (`idPlatform`);
+
+--
+-- Index pour la table `styles`
+--
+ALTER TABLE `styles`
+  ADD PRIMARY KEY (`idStyle`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`idUser`),
+  ADD UNIQUE KEY `pseudo` (`pseudo`),
+  ADD UNIQUE KEY `password` (`password`);
+
+--
+-- Index pour la table `users_platforms`
+--
+ALTER TABLE `users_platforms`
+  ADD KEY `idUser` (`idUser`),
+  ADD KEY `idPlatform` (`idPlatform`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `platforms`
+--
+ALTER TABLE `platforms`
+  MODIFY `idPlatform` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `styles`
+--
+ALTER TABLE `styles`
+  MODIFY `idStyle` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `users_platforms`
+--
+ALTER TABLE `users_platforms`
+  ADD CONSTRAINT `users_platforms_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `users_platforms_ibfk_2` FOREIGN KEY (`idPlatform`) REFERENCES `platforms` (`idPlatform`) ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
