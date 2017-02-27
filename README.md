1a6c2c4e14fd0f939c04adcd288ab1042fd5a9d8
david <ramenattedavid@hotmail.fr>
2017-02-27 17:32:46 +0100

    version V 1.0.3

---
+++ README.md
@@ -0,0 +1,2 @@
+base doc
+attention tout n'est pas forcement bon à prendre xD
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> 
+		<title>Vod-In</title>
+		<link rel="stylesheet" href="css/materialize.min.css">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" href="css/style.css">
+
+	</head>
+
+	<body>
+		<header>
+			<div class="navbar-fixed">
+				<nav>
+					<div class="nav-wrapper teal white">
+						 <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+						<a href="#" class="brand-logo"><div id="logo"></div></a>
+						<ul id="nav-mobile" class="right hide-on-med-and-down">
+							<li><a class="waves-effect waves-light btn green darken-4">Connexion</a></li>
+
+						</ul>
+						<div id="searchNav" class="nav-wrapper container flex">
+							<form id="searchBar">
+								<div id="searchChamp" class="input-field">
+									<input id="search" type="search" placeholder="  Recherche.." required>
+									<label class="label-icon" for="search"></label>
+								</div>
+							</form>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</header>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<script src="js/materialize.min.js"></script>
+		<script src="js/script.js"></script>
+	</body>
+</html>
