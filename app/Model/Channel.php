963d06952624c601878d513e1a43a46fa6426f8b
Olivier Marino <olivier.marino.om@gmail.com>
2017-03-02 15:28:00 +0100

    Api return hard coded values

--- app/Model/Channel.php
+++ app/Model/Channel.php
@@ -3,18 +3,25 @@
 namespace Model;
 
 class Channel {
-	private $uuid;
-	private $title;
-	private $avatar;
+    private $uuid;
+    private $title;
+    private $avatar;
     private $platform;
-	private $videos;
+    private $videos;
 
-	public function getUuid()      { return $this->uuid; }
-	public function setUuid($uuid) { $this->uuid = $uuid; }
-	public function getTitle()       { return $this->title; }
-	public function setTitle($title) { $this->title = $title; }
-	public function getAvatar()        { return $this->avatar; }
-	public function setAvatar($avatar) { $this->avatar = $avatar; }
+    public function __construct($uuid, $title, $avatar, $platform) {
+        $this->setUuid($uuid);
+        $this->setTitle($title);
+        $this->setAvatar($avatar);
+        $this->setPlatform($platform);
+    }
+
+    public function getUuid()      { return $this->uuid; }
+    public function setUuid($uuid) { $this->uuid = $uuid; }
+    public function getTitle()       { return $this->title; }
+    public function setTitle($title) { $this->title = $title; }
+    public function getAvatar()        { return $this->avatar; }
+    public function setAvatar($avatar) { $this->avatar = $avatar; }
     public function getPlatform()          { return $this->platform; }
     public function setPlatform($platform) { $this->platform = $platform; }
 
