babdb86fbf1fc05875a7c18f08e7ba1c59ca986f
Olivier Marino <olivier.marino.om@gmail.com>
2017-03-02 10:15:49 +0100

    Complete Channel/Video model + getter setter

--- app/Model/Channel.php
+++ app/Model/Channel.php
@@ -1,10 +1,23 @@
 <?php
 
-namespace \Croma;
+namespace Model;
 
 class Channel {
 	private $uuid;
-	private $label;
+	private $title;
 	private $avatar;
+    private $platform;
 	private $videos;
+
+	public function getUuid()      { return $this->uuid; }
+	public function setUuid($uuid) { $this->uuid = $uuid; }
+	public function getTitle()       { return $this->title; }
+	public function setTitle($title) { $this->title = $title; }
+	public function getAvatar()        { return $this->avatar; }
+	public function setAvatar($avatar) { $this->avatar = $avatar; }
+    public function getPlatform()          { return $this->platform; }
+    public function setPlatform($platform) { $this->platform = $platform; }
+
+    public function getVideos() { return $this->videos; }
+    public function addVideo($video) { $this->videos[$video->getUuid()] = $video; }
 }
