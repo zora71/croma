a5b57654710c9c8dcc91bd6f7694c48745be17ae
david <ramenattedavid@hotmail.fr>
2017-03-07 11:11:10 +0100

    Suppression des fichiers inutiles

--- app/Views/default/home2.php
+++
@@ -1,6 +0,0 @@
-<?php $this->layout('backend', ['title' => 'Accueil'] )?>
-
-<?php $this->start('main_content') ?>
-	<h2>Vod in. zora isa</h2>
-	<p>Vous avez atteint la page d'accueil. Bravo.</p>
-<?php $this->stop('main_content') ?>
--- app/Views/default/index2.php
+++
@@ -1,5 +0,0 @@
-<?php $this->layout('backend', ['title'=>'Accueil']) ?>
-
-<?php $this->start('main-content') ?>
-<h1>Bonjour Zora</h1>
-<?php $this->stop('main-content') ?>
