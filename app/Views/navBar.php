dd5781155d250527b35841735f47c38abe4587bc
david <ramenattedavid@hotmail.fr>
2017-03-07 10:58:13 +0100

    ajout verif connexion

--- app/Views/navBar.php
+++ app/Views/navBar.php
@@ -12,7 +12,20 @@ if(isset($_SESSION['error'])) {
                 <a href="index.php" class="brand-logo"><div id="logo"></div></a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
 
-                    <?php if (isset($_SESSION['user'])) : ?>
+                    <?php if (isset($_SESSION['user']['pseudo']) &&
+                              isset($_SESSION['user']['email']) &&
+                              isset($_SESSION['user']['uuid']) &&
+                              $_SESSION['user']['email'] == $_SESSION['user']['uuid'] &&
+                              $_SESSION['user']['pseudo'] == $_SESSION['user']['uuid']): ?>
+
+                    <!-- Modal Trigger -->
+                    <li><a class="waves-effect waves-light btn green darken-4 tooltipped" href="#modal1" data-position="bottom" data-delay="50" data-tooltip="Préférences">
+                        <i class="material-icons">mode_edit</i></a></li>
+                    <!-- bouton de connexion -->
+                    <li><a href="#modal2" class="waves-effect waves-light btn green darken-4 tooltipped" data-position="bottom" data-delay="50" data-tooltip="Connexion">Connexion</a></li>
+                    
+                    <?php else : ?>
+                    
                     <!-- Modal Trigger -->
                     <li> <div class="chip">
                         <?php echo $_SESSION['user']['pseudo']; ?>
@@ -21,13 +34,6 @@ if(isset($_SESSION['error'])) {
                         <i class="material-icons">mode_edit</i></a></li>
                     <!-- bouton de déconnexion -->
                     <li><a href="<?= $this->url('default_deconnexion') ?>" class="waves-effect waves-light btn red darken-4 tooltipped" data-position="bottom" data-delay="50" data-tooltip="Déconnexion">Déconnexion</a></li>
-
-                    <?php else : ?>
-                    <!-- Modal Trigger -->
-                    <li><a class="waves-effect waves-light btn green darken-4 tooltipped" href="#modal1" data-position="bottom" data-delay="50" data-tooltip="Préférences">
-                        <i class="material-icons">mode_edit</i></a></li>
-                    <!-- bouton de connexion -->
-                    <li><a href="#modal2" class="waves-effect waves-light btn green darken-4 tooltipped" data-position="bottom" data-delay="50" data-tooltip="Connexion">Connexion</a></li>
 
                     <?php endif; ?>
 
